da9c514895f90d91daf8017e242792427ae35e75
Lisa Ridley <lisa.ridley@planleft.com>
2023-08-25 17:59:19 -0400

    Issue #11: Removing docroot/modules/contrib folder from code repository, this is managed by Composer

--- docroot/modules/contrib/search_api_solr/src/Event/PreQueryEvent.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Drupal\search_api_solr\Event;
-
-/**
- * Event to be fired before the Search API query gets finally converted into a solarium query.
- */
-final class PreQueryEvent extends AbstractSearchApiQuerySolariumQueryEvent {}
