3c5a0cfbce1c0f9b2ef62c5548547d79d1b12009
Nick Vanpraet <nick.vanpraet@dropsolid.com>
2023-09-04 17:08:00 +0200

    Keep perms, etc. Add _format query param to build URL

--- .github/scripts/create_qa_branch_for_pr.sh
+++ .github/scripts/create_qa_branch_for_pr.sh
@@ -8,7 +8,7 @@ git config user.name github-actions
 git config user.email github-actions@github.com
 git checkout -b internal/qa-${QA_ENV}/PR-${PR_NUMBER}/${GITHUB_HEAD_REF}
 cd ..
-rsync -avz --delete --no-perms --no-owner --no-group --exclude=.git --exclude=.github --exclude=.gitlab-ci.yml pr/ main
+rsync -avz --delete --exclude=.git --exclude=.github --exclude=.gitlab-ci.yml pr/ main
 cd main
 git add .
 git commit -m "Created QA branch for PR ${PR_NUMBER}"
