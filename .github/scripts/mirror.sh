0f51699ea1ab29aa06fa683a0f1ca43d5ffcf24d
Nick Vanpraet <nick.vanpraet@dropsolid.com>
2024-10-30 17:11:10 +0100

    Update delete mirror logic

--- .github/scripts/mirror.sh
+++ .github/scripts/mirror.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-echo "${GITHUB_EVENT_NAME}"
-echo "${GITHUB_REF}"
-echo "github.event.ref: $GH_EV_RF"
-echo "github.event.ref_type: $GH_EV_RF_T"
+echo "Event: ${GITHUB_EVENT_NAME}"
+echo "Triggering git reference: ${GITHUB_REF}"
+echo "Triggering git reference for push events: ${GH_REF}"
+echo "Triggering git reference type: ${GH_REF_TYPE}"
 
 
 function git-setup() {
@@ -35,16 +35,8 @@ elif test "$GITHUB_EVENT_NAME" == "workflow_dispatch"; then
   git push mirror ${GITHUB_REF}:${GITHUB_REF} --force --tags
   git remote remove mirror
 elif test "$GITHUB_EVENT_NAME" == "delete"; then
-  if test "$DELETED_REF_TYPE" == "tag"; then
-    FULL_DELETED_REF="refs/tags/$DELETED_REF"
-  elif test "$DELETED_REF_TYPE" == "branch"; then
-    FULL_DELETED_REF="refs/heads/$DELETED_REF"
-  else
-    echo "Unexpected DELETED_REF_TYPE=$DELETED_REF_TYPE, expected 'branch' or 'tag'"
-    exit 1
-  fi
   git-setup
-  git push mirror :${FULL_DELETED_REF}
+  git push mirror :${GH_REF}
   git remote remove mirror
 else
   echo "Got unexpected GITHUB_EVENT_NAME: ${GITHUB_EVENT_NAME}"
